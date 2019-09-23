632b30432f5f86eea09c2203645da870c53cfb4b
jbpratt78 <jbpratt78@gmail.com>
2019-09-22 21:53:28 -0500

    more aka work

--- akas.go
+++ akas.go
@@ -6,73 +6,124 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sort"
 
 	"github.com/couchbase/vellum"
+	"github.com/pkg/errors"
 )
 
 const AKAS = "akas.fst"
 
-type aka struct {
-	rdr *csv.Reader
+type akas struct {
+	idx *csv.Reader
+	fst *vellum.FST
 }
 
-func (*aka) open() error {
-	return nil
+type akaRecord struct {
+	pos uint64
+	rec string
 }
 
-// create an AKA index by reading the AKA recs
-// from a given directory and writing to the corresponding
-// index directory
-func (a *aka) create(P1, P2 string) error {
+func akaOpen(P1, P2 string) (*akas, error) {
 	f, err := os.Open(P1)
 	if err != nil {
-		panic(err)
+		return nil, errors.Wrap(err, "failed to open tsv")
 	}
-	a.rdr = csv.NewReader(f)
-	a.rdr.LazyQuotes = true
-	a.rdr.FieldsPerRecord = -1
-	a.rdr.Comma = '\t'
+	r := csv.NewReader(f)
+	r.LazyQuotes = true
+	r.FieldsPerRecord = -1
+	r.Comma = '\t'
+
+	fst, err := vellum.Open(filepath.Join(P2, AKAS))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open fst")
+	}
+
+	return &akas{idx: r, fst: fst}, nil
+}
+
+// create an akas index by reading the akas recs
+// from a given directory and writing to the corresponding
+// index directory
+func akaCreate(P1, P2 string) (*akas, error) {
+	f, err := os.Open(P1)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open tsv")
+	}
+	r := csv.NewReader(f)
+	r.LazyQuotes = true
+	r.FieldsPerRecord = -1
+	r.Comma = '\t'
 
 	f, err = os.Create(filepath.Join(P2, AKAS))
 	if err != nil {
-		return err
+		return nil, errors.Wrap(err, "failed to create index file")
 	}
 	defer f.Close()
 
 	builder, err := vellum.New(f, nil)
 	if err != nil {
-		return err
+		return nil, errors.Wrap(err, "failed to create index builder")
 	}
 	defer builder.Close()
 
 	var count uint64 = 0x64
 	//sortTsv(recs
 
+	outRecords := []akaRecord{}
 	// currently broken due to sort
 	// What if we read the records into a temp
 	// struct slice, storing the offset with it
+	// sort this slice
 	// and then use builder.Insert in a second loop?
 	for {
-		record, err := a.rdr.Read()
+		rec, err := r.Read()
 		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 
 		pos, err := f.Seek(0, 1)
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 		}
 
-		err = builder.Insert([]byte(record[0]), uint64(pos)<<count|uint64(pos))
+		outRecords = append(outRecords, akaRecord{uint64(pos), rec[0]})
+	}
+
+	sortSlice(outRecords)
+
+	for _, x := range outRecords {
+		err = builder.Insert([]byte(x.rec), x.pos<<count|x.pos)
 		if err != nil {
 			log.Println(err)
 		}
 
-		count += uint64(pos)
+		count += uint64(x.pos)
 	}
 
-	return nil
+	return akaOpen(P1, P2)
+}
+
+func (a *akas) find(id string) error {
+
+	v, ex, err := a.fst.Get([]byte(id))
+	if err != nil {
+		return errors.Wrap(err, "failed to get value")
+	}
+	if ex {
+		count := v >> 48
+		offset := v & ((1 << 48) - 1)
+		_ = count
+		_ = offset
+	}
+	return errors.New("not implemented")
+}
+
+func sortSlice(data []akaRecord) {
+	sort.Slice(data, func(i, j int) bool {
+		return data[i].rec < data[j].rec
+	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jbpratt78/imdb-index
 
 go 1.12
 
-require github.com/couchbase/vellum v0.0.0-20190829182332-ef2e028c01fd
+require (
+	github.com/couchbase/vellum v0.0.0-20190829182332-ef2e028c01fd
+	github.com/pkg/errors v0.8.1
+)
