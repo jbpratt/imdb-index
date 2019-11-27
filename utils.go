170b4d410e447d2b293f1d59831337a8c44f9ccd
jbpratt78 <jbpratt78@gmail.com>
2019-11-26 21:33:40 -0600

    started dl util

--- utils.go
+++ utils.go
@@ -20,6 +20,7 @@ import (
 // TODO: just return the file and let caller handle
 // this will allow the caller to create a new reader
 // after seeking to the desired position
+// rename to openFile?
 func OpenTsv(path string) (*csv.Reader, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -31,4 +32,49 @@ func OpenTsv(path string) (*csv.Reader, error) {
 	r.Comma = '\t'
 
 	return r, nil
+}
+
+func download_all() error {
+	dataSets := []string{
+		"title.akas.tsv.gz",
+		"title.basics.tsv.gz",
+		"title.episode.tsv.gz",
+		"title.ratings.tsv.gz",
+	}
+
+	// make dir
+
+	// loop over data sets and download one at a time
+	for _, set := range dataSets {
+		_ = set
+	}
+
+	return nil
+}
+
+/// Downloads a single data set, decompresses it and writes it to the
+/// corresponding file path in the given directory.
+func download() error {
+
+	// create file
+
+	// format url
+
+	// make request
+
+	// decode Gz file
+
+	// sort and write
+
+	return nil
+}
+
+/// Read all CSV data into memory and sort the records in lexicographic order.
+///
+/// This is unfortunately necessary because the IMDb data is no longer sorted
+/// in lexicographic order with respect to the `tt` identifiers. This appears
+/// to be fallout as a result of adding 10 character identifiers (previously,
+/// only 9 character identifiers were used).
+func writeSortedCSVRecords() error {
+	return nil
 }
