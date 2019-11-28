0116fff0cfe1c96b8f96e18870e5892d47044601
jbpratt78 <jbpratt78@gmail.com>
2019-11-27 20:05:29 -0600

    working on dl

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
+	if _, err := os.Stat("./data"); os.IsNotExist(err) {
+		err = downloadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	// check if path exists
 	err := createAka()
 	if err != nil {
--- utils.go
+++ utils.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"compress/gzip"
 	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
+	"path"
+	"sort"
+	"strings"
 )
+
+const IMDBBaseURL = "https://datasets.imdbws.com"
 
 /*func sortTsv(data [][]string) {
 	sort.Slice(data[:], func(i, j int) bool {
@@ -18,7 +29,7 @@ import (
 }*/
 
 // TODO: just return the file and let caller handle
-// this will allow the caller to create a new reader
+// this will allow the caller to create a new csv reader
 // after seeking to the desired position
 // rename to openFile?
 func OpenTsv(path string) (*csv.Reader, error) {
@@ -34,7 +45,15 @@ func OpenTsv(path string) (*csv.Reader, error) {
 	return r, nil
 }
 
-func download_all() error {
+func openFile(path string) (*os.File, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
+
+func downloadAll() error {
 	dataSets := []string{
 		"title.akas.tsv.gz",
 		"title.basics.tsv.gz",
@@ -43,10 +62,22 @@ func download_all() error {
 	}
 
 	// make dir
+	err := os.Mkdir("data", 0700)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.Mkdir("index", 0700)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// loop over data sets and download one at a time
 	for _, set := range dataSets {
-		_ = set
+		err = download(set)
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	return nil
@@ -54,17 +85,32 @@ func download_all() error {
 
 /// Downloads a single data set, decompresses it and writes it to the
 /// corresponding file path in the given directory.
-func download() error {
+func download(ds string) error {
 
-	// create file
+	// create out file
+	f, err := os.OpenFile(filenameWithoutExtension(ds), os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
 
-	// format url
-
-	// make request
+	resp, err := http.Get(fmt.Sprintf("%s/%s", IMDBBaseURL, ds))
+	if err != nil {
+		return err
+	}
 
 	// decode Gz file
+	r, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
 
 	// sort and write
+	err = writeSortedCSVRecords(r, f)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -75,6 +121,18 @@ func download() error {
 /// in lexicographic order with respect to the `tt` identifiers. This appears
 /// to be fallout as a result of adding 10 character identifiers (previously,
 /// only 9 character identifiers were used).
-func writeSortedCSVRecords() error {
-	return nil
+func writeSortedCSVRecords(in io.Reader, out io.Writer) error {
+	// We actually only sort the raw lines here instead of parsing CSV records,
+	// since parsing into CSV records has fairly substantial memory overhead.
+	// Since IMDb CSV data never contains a record that spans multiple lines,
+	// this transformation is okay.
+	scanner := bufio.NewScanner(in)
+	data := scanner.Bytes()
+	sort.Slice(data, func(i int, j int) bool { return data[i] < data[j] })
+	_, err := out.Write(data)
+	return err
+}
+
+func filenameWithoutExtension(fn string) string {
+	return strings.TrimSuffix(fn, path.Ext(fn))
 }
